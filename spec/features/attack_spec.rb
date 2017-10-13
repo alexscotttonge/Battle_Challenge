23569f1719486c5f30c604749ff8558dd7c3d736
Alex <alex@scotttonge.com>
2017-10-13 16:51:21 +0100

    Added some CSS and extra functionality for randomly assigning hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,8 @@ feature 'attack gameplay working' do
   scenario 'confirm player 1 has attacked player 2' do
     sign_in_and_play
     click_button ('Player 1 attack Player 2!')
-    expect(page).to have_content('You have boshed Player 2 and reduced hit points by 10')
+    hitpoints = find('span.random_number').text.to_i
+    expect(page).to have_content('You have boshed Player 2 and reduced hit points by ' + hitpoints.to_s)
   end
 
   scenario 'reduce Player 2 hit points by the right number' do
