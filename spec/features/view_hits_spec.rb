d243102440127e27bea69f44a28191b3e7455c1a
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-13 10:41:32 +0100

    Added sign in and play method to web helpers file

--- spec/features/view_hits_spec.rb
+++ spec/features/view_hits_spec.rb
@@ -1,10 +1,7 @@
 
 feature 'view hits on player2' do
   scenario 'Can view hit points on player 2' do
-    visit ('/')
-    fill_in :name1, with: 'Alex'
-    fill_in :name2, with: 'David'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content('Player 2 has loads of hit points')
   end
 end
